d4ef1c03e4a0cb68384826c70cb0a649d98b495a
ishanrana363 <ishanrana363@gmail.com>
2024-10-26 23:44:25 +0600

    react helmet added

--- src/pages/logo-page/AllLogoPage.jsx
+++ src/pages/logo-page/AllLogoPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import AllLogo from '../../component/logo/AllLogo'
+import { Helmet } from 'react-helmet-async';
 
 const AllLogoPage = () => {
   return (
     <div>
+      <>
+      <Helmet>
+        <title>Dashboard | All Logo</title>
+      </Helmet>
+      </>
       <AllLogo></AllLogo>
     </div>
   )
--- src/pages/logo-page/UploadLogoPage.jsx
+++ src/pages/logo-page/UploadLogoPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import UploadLogo from '../../component/logo/UploadLogo'
+import { Helmet } from 'react-helmet-async'
 
 const UploadLogoPage = () => {
   return (
     <div>
+      <>
+      <Helmet>
+        <title>Dashboard | Upload Logo</title>
+      </Helmet>
+      </>
       <UploadLogo></UploadLogo>
     </div>
   )
